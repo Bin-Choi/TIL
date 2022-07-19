cd4baf8a2326b5cb71582e8f8e57e09b27a16502
Bin-Choi <csb5268@gmail.com>
2022-07-20 00:55:29 +0900

    1~20번

---
+++ basic100/basic100.py
@@ -0,0 +1,63 @@
+#6002
+# print("Hello World")
+#6003
+# print("Hello")
+# # print("World")
+#6004
+# print("'Hello'")
+# 6005
+# print('"Hello World"')
+# 6006
+# print(''' "!@#$%^&*()' ''')
+# 6007
+# print(''' "C:\\Download\\'hello'.py" ''')
+# 6008
+# print('print("Hello\\nWorld")')
+# 6009
+# c = input()
+# print(c)
+# 6010
+# n = input()
+# n = int(n)
+# print(n)
+# 6011
+# f = input()
+# f = float(f)
+# print(f)
+# 6012
+# a = input()
+# b = input()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+# 6013
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+# 6014
+# n = input()
+# for i in range(3):
+#     print(n) 
+# 6015
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a)
+# print(b)
+# 6016
+# c1,c2 = input().split()
+# print(c2,c1)
+# 6017
+# a = input()
+# print(a, a, a)
+# 6018
+# a = input()
+# print(a)
+# 6019
+# y,m,d = input().split('.')
+# print(f'{d}-{m}-{y}')
+6020
+a,b = input().split('-')
+print(a+b)
