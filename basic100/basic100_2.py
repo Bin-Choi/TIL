9736665945b7a868988de0cc0fad997feba04465
Bin-Choi <csb5268@gmail.com>
2022-07-29 18:04:17 +0900

    basic 100

---
+++ basic100/basic100_2.py
@@ -0,0 +1,175 @@
+#6021 ~ 
+
+# s = input()
+# for c in s:
+#     print(c)
+
+# s = input()
+# print(s[:2], s[2:4], s[4:6])
+
+# s = input()
+# print(s.split(':')[1])
+
+# w1, w2 = input().split()
+# s = w1 + w2
+# print(s)
+
+# a,b = input().split()
+# c = int(a) + int(b)
+# print(c)
+
+# a = input()
+# b = input()
+# c = float(a) + float(b)
+# print(c)
+
+# a = input()
+# n = int(a)
+# print('%x'%n)
+
+# a = input()
+# n = int(a)
+# print('%X'%n)
+
+# a = input()
+# n = int(a,16)
+# print('%o'%n)
+
+# n = ord(input())
+# print(n)
+
+# c = int(input())
+# print(chr(c))
+
+# c = int(input())
+# print(-c)
+
+# c = input()
+# if type(c) == int:
+#     print(c+1)
+# elif type(c) == str:
+#     d =ord(c)
+#     print(chr(d+1))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# c = a - b
+# print(abs(c))
+
+# f1,f2 = input().split()
+# m = float(f1) * float(f2)
+# print(m)
+
+# w, n = input().split()
+# print(w*int(n))
+
+# n = int(input())
+# s = input()
+# print(s*n)
+
+# a,b = input().split()
+# print (int(a)**int(b))
+
+# f1,f2 = input().split()
+# f1 = float(f1)
+# f2 = float(f2)
+# print(f1**f2)
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a//b)
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a%b)
+
+# a = input()
+# a = float(a)
+# print(format(a,".2f"))
+
+# a,b = input().split()
+# a = float(a)
+# b = float(b)
+# print(format(a/b,".3f"))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+# print(format(a/b,".2f"))
+
+# a,b,c = input().split()
+
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = a+b+c
+# print(d, format(d/3,".2f"))
+
+# a = int(input())
+# print(a<<1)
+# print(a*2)
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a*(2**b))
+
+# a,b = input().split()
+# a=int(a)
+# b=int(b)
+# print(a<b)
+
+# a,b = input().split()
+# print(a == b)
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a<b or a == b)
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a !=b)
+
+# a = int(input())
+# print(bool(a))
+
+# n = int(input())
+# print(not(bool(n)))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(bool(a)and bool(b))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(bool(a) or bool(b))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(bool(a) != bool(b))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(bool(a) == bool(b))
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+# print(not(bool(a)) and not(bool(b)))
+
+
+
